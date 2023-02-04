c21a88b6716e9211caf13dd8ec67240d9d31e666
Hugo Weichert <105549067+h-weichert@users.noreply.github.com>
2023-02-04 17:48:35 +0400

    Update GameManager.cs
    - Score + HighScore System implemented
    - Game Over State ready for implementation. Just make bool == True :+1:

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,13 +12,21 @@ public class GameManager : MonoBehaviour
     public float speed = 1.0f;
     public float bkgSpeed = 1.2f;
     public float speedIncrement = 0.1f;
-
+    public float scoreFloat;
+    public int score;
+    public int highscore;
+    public bool isDead;
+    
+    
 
     private GameObject[] backgroundInstance;
     private GameObject[] foregroundInstance;
 
     void Start()
     {
+        Load();
+        isDead = false;
+        scoreFloat = 0.0f;
         backgroundInstance = new GameObject [bkgSpawnPoints.Length];
         for (int i = 0; i <bkgSpawnPoints.Length; i++)
         {
@@ -38,16 +47,51 @@ public class GameManager : MonoBehaviour
             backgroundInstance[i].transform.position -= new Vector3(0, bkgSpeed * Time.deltaTime, 0);
 
         }
-            bkgSpeed += speedIncrement * Time.deltaTime;
-        
+        bkgSpeed += speedIncrement * Time.deltaTime;
+
         for (int i = 0; i < wallSpawnPoints.Length; i++)
         {
             foregroundInstance[i].transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
 
         }
 
-            speed += speedIncrement * Time.deltaTime;
+        speed += speedIncrement * Time.deltaTime;
 
+        
+        
+
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetInt("highscore", highscore);
+    }
+
+    public void Load()
+    {
+        highscore = PlayerPrefs.GetInt("highscore");
+    }
+
+    private void FixedUpdate()
+    {
+
+        scoreFloat += Time.deltaTime;
+        int score = (int)scoreFloat * 7;
+        Debug.Log("Score:" + score);
+
+        if (isDead == true) //Use this to end game
+        {
+            Save();
+        
+            if (score > highscore)
+            {
+                highscore = score;
+            }
+            Debug.Log("GAME OVER");
+            Debug.Log("Highscore: " + highscore);
+
+        }
+
+        
 
     }
 

d2f2fbea65f7a6ff829f7aafca11aadc40652591
Hugo Weichert <105549067+h-weichert@users.noreply.github.com>
2023-02-05 03:05:25 +0400

    Score now shows
    Next step is to add highscore underneath but im too tired to do that now

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public float scoreFloat;
     public int score;
     public int highscore;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         highscore= 0;
+        scoreText = GetComponent<TextMeshProUGUI>();
+        //UpdateScore();
     }
 
+    private void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
     // Update is called once per frame
     private void FixedUpdate()
     {
 
         scoreFloat += Time.deltaTime;
-        int score = (int)scoreFloat * 7;
+        score = (int)scoreFloat * 7;
+        UpdateScore();  
+
         //Debug.Log("Score:" + score);
 
         DeadTrigger script = gameObject.GetComponent<DeadTrigger>();
@@ -28,10 +39,7 @@ public class ScoreManager : MonoBehaviour
 
             if (script.isDead)
             {
-
-
-                
-
+                           
                 if (script.isDead == true) 
                 {
                     Save();

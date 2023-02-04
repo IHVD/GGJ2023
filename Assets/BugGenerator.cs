b8eb10ead80cd09276aac741485c27e7e765e6cf
Hugo Weichert <105549067+h-weichert@users.noreply.github.com>
2023-02-05 01:21:55 +0400

    Added Bug obstacle
    Needs full implementation for randomizer

---
+++ Assets/BugGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    public float pointAX;
+    public float pointBX;
+    public float yMin = -1f;
+    public float yMax = 1f;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        Vector3 startingPoint = new Vector3(pointAX, Random.Range(yMin, yMax), 0);
+        Vector3 targetPoint = startingPoint.x == pointAX ? new Vector3(pointBX, Random.Range(yMin, yMax), 0) : new Vector3(pointAX, Random.Range(yMin, yMax), 0);
+
+        Debug.Log(startingPoint+ " " + targetPoint);
+        // Instantiate the prefab at the starting point
+        GameObject instance = Instantiate(prefab, startingPoint, Quaternion.identity);
+
+        // Start the movement coroutine
+        StartCoroutine(MoveFromPointToPoint(instance, startingPoint, targetPoint));
+    }
+
+    private IEnumerator MoveFromPointToPoint(GameObject instance, Vector3 startingPoint, Vector3 targetPoint)
+    {
+        while (instance.transform.position != targetPoint)
+        {
+            instance.transform.position = Vector3.MoveTowards(instance.transform.position, targetPoint, Time.deltaTime * speed);
+            yield return null;
+        }
+
+        Destroy(instance);
+    }
+}
+
